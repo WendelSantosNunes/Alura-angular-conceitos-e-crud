cb27232ac5d203a29ac8b73fd5a1ed2d3e2d925c
wendel dos santos nunes <wendelnunes9999@gmail.com>
2024-07-16 19:43:32 -0300

    An if was added to check whether or not you have thoughts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component'
 import { RodapeComponent } from './componentes/rodape/rodape.component';
 import { ListarPensamentoComponent } from './componentes/pensamentos/listar-pensamento/listar-pensamento.component';
 import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
+import { PensamentoComponent } from './componentes/pensamentos/pensamento/pensamento.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensam
     CabecalhoComponent,
     RodapeComponent,
     ListarPensamentoComponent,
-    CriarPensamentoComponent
+    CriarPensamentoComponent,
+    PensamentoComponent
   ],
   imports: [
     BrowserModule,
